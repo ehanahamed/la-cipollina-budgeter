68dddbc406e1392160a5b5c1b3b16af5e1e94c8a
Ehan <ehanahamed@ehan.dev>
2025-06-03 13:29:31 +0000

    setup libraries + stuff

---
+++ api/src/main/java/com/lacipollina/bugeter/BugeterApplication.java
@@ -0,0 +1,13 @@
+package com.lacipollina.bugeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugeterApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/lacipollina/bugeter/BugeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacipollina.bugeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
