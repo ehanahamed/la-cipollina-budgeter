7797c783dfcabe2a593b039407396891f4f43250
Ehan <ehanahamed@ehan.dev>
2025-06-06 09:48:49 -0400

    work in progress

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -19,12 +19,11 @@
     <img src="https://lacipollina.com/content/website/accounts/1298/240-476aee843252af73ec11abdffa5aff45.png">
 </div>
 <h1 class="h3 header-text">Budgeter</h1>
-<tag attribute="asdfjkl">text</tag>
 <div class="flex center">
     <div class="flex center" style="flex-direction: column;">
         <div class="combo-select">
-            <button class="left" href="/edit-budget">Edit Budget</button>
-            <button class="mid" href="/edit-budget">Run Day</button>
+            <a class="button left" href="/edit-budget">Edit Budget</a>
+            <a class="button mid" href="/run-day">Run Day</a>
             <button class="right">Edit Day</button>
         </div>
         <a href="/reports" class="button">Reports</a>
