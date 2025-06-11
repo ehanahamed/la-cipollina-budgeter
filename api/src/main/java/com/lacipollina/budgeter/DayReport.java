33860056fc3ea1f322805560420b53d13f3cf848
cristianhdez226 <crisemeportillo@gmail.com>
2025-06-11 12:37:50 +0000

    huei

--- api/src/main/java/com/lacipollina/budgeter/DayReport.java
+++ api/src/main/java/com/lacipollina/budgeter/DayReport.java
@@ -14,6 +14,80 @@ public class DayReport {
 
     public DayReport () 
     {
-        
+
+    }
+
+    public void setFoodCostInital(float n){
+        foodCostInital=n;
+    }
+    public void setKitchInital(float n){
+        kitchInital=n;
+    }
+    public void setFloorInital(float n){
+        floorInital=n;
+    }
+    public void setFoodCostIncs(float n){
+        foodCostIncs=n;
+    }
+    public void setKitchIncs(float n){
+        kitchIncs=n;
+    }
+    public void setFloorIncs(float n){
+        floorIncs=n;
+    }
+    public void setFoodExps(float n){
+        foodExps=n;
+    }
+    public void setKitchExps(float n){
+        kitchExps=n;
+    }
+    public void setFloorExps(float n){
+        floorExps=n;
+    }
+    public void setFoodCostFinal(float n){
+        foodCostFinal=n;
+    }
+    public void setKitchFinal(float n){
+        kitchFinal=n;
+    }
+    public void setFloorFinal(float n){
+        floorFinal=n;
+    }
+
+    public float getFoodCostInital(){
+        return foodCostInital;
+    }
+    public float getKitchInital(){
+        return kitchInital;
+    }
+    public float getFloorInital(){
+        return floorInital;
+    }
+    public float setFoodCostIncs(){
+        return foodCostIncs;
+    }
+    public float setKitchIncs(){
+        return kitchIncs;
+    }
+    public float setFloorIncs(){
+        return floorIncs;
+    }
+    public float setFoodExps(){
+        return foodExps;
+    }
+    public float setKitchExps(){
+        return kitchExps;
+    }
+    public float setFloorExps(){
+        return floorExps;
+    }
+    public float setFoodCostFinal(){
+        return foodCostFinal;
+    }
+    public float setKitchFinal(){
+        return kitchFinal;
+    }
+    public float setFloorFinal(){
+        return floorFinal;
     }
 }
