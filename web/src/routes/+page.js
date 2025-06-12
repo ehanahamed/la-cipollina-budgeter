2f46cd40dde252e8caca35182bdac76b5764f6c5
Ehan <ehanahamed@ehan.dev>
2025-06-11 22:34:02 -0400

    we might need to be done soon

--- web/src/routes/+page.js
+++ web/src/routes/+page.js
@@ -5,20 +5,20 @@ export async function load() {
         PUBLIC_API_URL: PUBLIC_API_URL
     }
     try {
-        if (localStorage.getItem("auth")) {
-            const req = await fetch(PUBLIC_API_URL + "/la-cipollina-budgeter/graphql", {
-                method: "POST",
-                headers: {
-                    "Authorization": "Bearer " + 
-                }
-                body: JSON.stringify({
-                    query: "{ amAuthed }"
-                })
-            })
-            if (req) {
-
-            }
-        }
+        // if (localStorage.getItem("auth")) {
+        //     const req = await fetch(PUBLIC_API_URL + "/la-cipollina-budgeter/graphql", {
+        //         method: "POST",
+        //         headers: {
+        //             "Authorization": "Bearer " + 
+        //         }
+        //         body: JSON.stringify({
+        //             query: "{ amAuthed }"
+        //         })
+        //     })
+        //     if (req) {
+        //
+        //     }
+        // }
     } finally {
         return result;
     }
