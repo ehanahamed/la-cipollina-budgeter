ca84d25e3b27d82d7cab4515fe2118d841cab0e6
Ehan <ehanahamed@ehan.dev>
2025-06-12 04:18:07 -0400

    update ui

--- web/src/routes/settings/+page.svelte
+++ web/src/routes/settings/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+import BackArrowIcon from "$lib/icons/BackArrow.svelte";
+import { base } from "$app/paths";
 let employees = $state([
     {
         name: "Carson",
@@ -17,6 +19,12 @@ let employees = $state([
     }
 ]);
 </script>
+<div class="grid page">
+    <div class="content">
+                <div style="margin-top: 1rem; margin-bottom: 1rem;"><a href={base} class="button faint">
+                    <BackArrowIcon></BackArrowIcon>
+                    Back
+                </a></div>
 Add/Edit Employees
 <table>
     <thead>
@@ -29,12 +37,47 @@ Add/Edit Employees
     <tbody>
         {#each employees as employee}
             <tr>
+                {#if employee.edit}
+                <td><input type="text" placeholder="Name" bind:value={employee.name} style="width: 8rem;"></td>
+                <td>
+                    <div class="select-wrapper" style="width: 8rem;">
+                        <select bind:value={employee.type}>
+                            <option value="floor">Floor</option>
+                            <option value="kitchen">Kitchen</option>
+                        </select>
+                    </div>
+                </td>
+                <td>
+                    <input type="text" placeholder="Name" bind:value={employee.wage} style="width: 8rem;">
+                </td>
+                <td><button class="alt" onclick={function () {
+                    employee.edit = false
+                }}>Save</button></td>
+                {:else}
                 <td>{employee.name}</td>
                 <td>{employee.type}</td>
+                <td>${employee.wage}/hr</td>
+                <td><button class="alt" onclick={function () {
+                    employee.edit = true
+                }}>Edit</button></td>
+                {/if}
             </tr>
         {/each}
         <tr>
+            <td>
+                <button onclick={function () {
+                    employees.push({
+                        name: "",
+                        type: "",
+                        wage: 0,
+                        edit: true
+                    })
+                }}>
+                    Add
+                </button>
+            </td>
         </tr>
     </tbody>
 </table>
-
+</div>
+</div>
