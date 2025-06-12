d3554afd99c2977dbb94887ee03de9b9e4b8873e
Ehan <ehanahamed@ehan.dev>
2025-06-11 20:55:05 -0400

    asdf

--- web/src/routes/+page.js
+++ web/src/routes/+page.js
@@ -1,7 +1,26 @@
 import { PUBLIC_API_URL } from "$env/static/public";
-export function load() {
-    return {
+export async function load() {
+    let result = {
+        authed: false,
         PUBLIC_API_URL: PUBLIC_API_URL
+    }
+    try {
+        if (localStorage.getItem("auth")) {
+            const req = await fetch(PUBLIC_API_URL + "/la-cipollina-budgeter/graphql", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + 
+                }
+                body: JSON.stringify({
+                    query: "{ amAuthed }"
+                })
+            })
+            if (req) {
+
+            }
+        }
+    } finally {
+        return result;
     }
 }
 
