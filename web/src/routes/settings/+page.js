22d07731fd3465a43f0dbb88f7d7a9dbaadc0c11
Ehan <ehanahamed@ehan.dev>
2025-06-23 15:29:06 +0000

    use fetch passed into load instead of browser fetch

--- web/src/routes/settings/+page.js
+++ web/src/routes/settings/+page.js
@@ -1,6 +1,6 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 
-export async function load() {
+export async function load({ fetch }) {
     if (window.localStorage) {
         let authToken = localStorage.getItem("auth");
         if (!authToken) {
