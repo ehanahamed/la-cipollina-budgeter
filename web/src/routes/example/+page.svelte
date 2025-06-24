fd695b1aa8e4b73b4a2689118037390167a54e29
Ehan <ehanahamed@ehan.dev>
2025-06-24 14:51:40 +0000

    rm example page

--- web/src/routes/example/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { base } from "$app/paths";
-</script>
-example
-<button onclick={function () {
-alert("abcdefg")
-}}>adsjfklfdsjlk</button>
-<a href={base} class="button">adsfjkl</a>
-
