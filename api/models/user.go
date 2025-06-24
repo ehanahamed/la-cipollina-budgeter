70df1b2e070f3f1240e475dbef719396b5175daa
Ehan <ehanahamed@ehan.dev>
2025-06-24 14:58:57 +0000

    Fix username in user model

--- api/models/user.go
+++ api/models/user.go
@@ -6,12 +6,12 @@ import (
 
 type User struct {
     ID         int       `db:"id" json:"id"`
-    Username   string    `db:"name" json:"name"`
+    Username   string    `db:"username" json:"username"`
     CreatedAt  time.Time `db:"created_at" json:"createdAt"`
     UpdatedAt  time.Time `db:"updated_at" json:"updatedAt"`
 }
 
 type NewUser struct {
-    Username   	string `db:"name" json:"name"`
+    Username   	string `db:"username" json:"username"`
 	NewPassword string `json:"newPassword"`
 }
