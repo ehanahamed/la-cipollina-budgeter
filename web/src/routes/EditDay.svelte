3ddec58c1e1edd5db46efee14e2be8d9db02be4e
Ehan <ehanahamed@ehan.dev>
2025-07-25 16:37:04 -0400

    update layout and parsing

--- web/src/routes/EditDay.svelte
+++ web/src/routes/EditDay.svelte
@@ -3,53 +3,86 @@ import { base } from "$app/paths";
 import BackArrowIcon from "$lib/icons/BackArrow.svelte";
 import PlusIcon from "$lib/icons/Plus.svelte";
 import CheckmarkIcon from "$lib/icons/Checkmark.svelte";
+import XMarkIcon from "$lib/icons/CloseXMark.svelte";
 import { onMount } from "svelte";
 let { data } = $props();
 let floorHoursArray = $state([]);
+let floorWorkedTodayArray = $state([]);
 let kitchenHoursArray = $state([]);
+let kitchenWorkedTodayArray = $state([]);
+const date = new Date()
+const weekDayName = [
+    "Monday", "Tuesday", "Wednesday", "Thrusday",
+    "Friday", "Saturday", "Sunday"
+][date.getDay()]
+const weekDayKey = [
+    "mon", "tue", "wed", "thu",
+    "fri", "sat", "sun"
+][date.getDay()]
 if (data.new) {
     data.employees.forEach(function (employee) {
         if (employee?.type == "FLOOR") {
-            floorHoursArray.push({
-                name: employee.name,
-                hours: ""
-            })
-        }
-        if (employee?.type == "KITCHEN") {
-            kitchenHoursArray.push({
-                name: employee.name,
-                hours: ""
-            })
+            if (employee?.specialPay) {
+                if (employee.specialPay[weekDayKey]?.perDay != null) {
+                    floorWorkedTodayArray.push({
+                        employee: employee,
+                        workedToday: false
+                    });
+                }
+                if (employee.specialPay[weekDayKey]?.perHour != null) {
+                    floorHoursArray.push({
+                        employee: employee,
+                        hours: ""
+                    });
+                }
+            } else {
+                floorHoursArray.push({
+                    employee: employee,
+                    hours: ""
+                });
+            }
+        } else if (employee?.type == "KITCHEN") {
+            if (employee?.specialPay) {
+                if (employee.specialPay[weekDayKey]?.perDay != null) {
+                    kitchenWorkedTodayArray.push({
+                        employee: employee,
+                        workedToday: false
+                    });
+                }
+                if (employee.specialPay[weekDayKey]?.perHour != null) {
+                    kitchenHoursArray.push({
+                        employee: employee,
+                        hours: ""
+                    });
+                }
+            } else {
+                kitchenHoursArray.push({
+                    employee: employee,
+                    hours: ""
+                });
+            }
         }
     })
 }
-let ingredientExpensesArray = $state([
+let foodCostsArray = $state([
     {
-        increase: "",
-        decrease: ""
+        cost: "",
+        notes: ""
     }
 ]);
-let dayOfTheWeek = $state();
-let weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thrusday",
-    "Friday",
-    "Saturday"
-]
-let showSetBudget = $state(false);
-onMount(function () {
-    let today = new Date()
-    dayOfTheWeek = weekDays[today.getDay()]
-    if (
-        dayOfTheWeek == "Monday" ||
-        dayOfTheWeek == "Tuesday" ||
-        dayOfTheWeek == "Wednesday"
-    ) {
-        showSetBudget = true;
-    }
+let foodCostsTotal = $derived.by(() => {
+    let total = 0;
+    foodCostsArray.forEach((row) => {
+        if (typeof row.cost === "string") {
+            const cost = parseFloat(row.cost.replaceAll(",", ""));
+            if (!isNaN(cost)) {
+                total += cost;
+            }
+        } else if (typeof row.cost === "number") {
+            total += row.cost;
+        }
+    })
+    return total;
 })
 </script>
 <style>
@@ -86,9 +119,6 @@ onMount(function () {
 .gridtablelayout > .areasave {
     grid-area: areasave;
     justify-self: start;
-}
-.gridtablelayout input[type=text] {
-    width: 8rem;
 }
 .centerbutnotonmobile {
     text-align: center;
@@ -131,27 +161,13 @@ onMount(function () {
                 {:else}
                     <p>Edit Day</p>
                 {/if}
-                <h3 style="margin-top: 0px;">{dayOfTheWeek}</h3>
+                <h3 style="margin-top: 0.2rem; margin-bottom: 0px;">{weekDayName}</h3>
+                <p style="margin-top: 0.2rem;">{[
+                    "January", "February", "March", "April",
+                    "May", "June", "July", "August", "September",
+                    "October", "November", "December"
+                ][date.getMonth()]} {date.getDate()}, {date.getFullYear()}</p>
             </div>
-            {#if showSetBudget}
-                <div>
-                    Set budget for this week:
-                    <table class="setbudgettable">
-                        <thead>
-                            <tr>
-                                <th>Floor Pay</th>
-                                <th>Kitchen Pay</th>
-                                <th>Food Cost</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><input type="text"></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            {/if}
         </div>
         <div class="gridtablelayout">
             <div class="areaback">
@@ -161,7 +177,12 @@ onMount(function () {
                 </a>
             </div>
             <div class="areafloor">
-                <p>Floor Workers</p>
+                <div class="flex" style="justify-content: space-between;">
+                    <p>Floor Workers</p>
+                    <p class="fg0">
+                        {floorHoursArray.length + floorWorkedTodayArray.length} total
+                    </p>
+                </div>
                 <table>
                     <thead>
                         <tr>
@@ -172,15 +193,50 @@ onMount(function () {
                     <tbody>
                         {#each floorHoursArray as row}
                             <tr>
-                                <td>{row.name}</td>
-                                <td><input type="text" bind:value={row.hours}></td>
+                                <td>{row.employee.name}</td>
+                                <td><input type="text" bind:value={row.hours}
+                                    style="min-width: 6rem; field-sizing: content;"></td>
+                            </tr>
+                        {/each}
+                        {#if floorWorkedTodayArray.length >= 1}
+                        <tr>
+                            <th>Employee</th>
+                            <th>Worked Today?</th>
+                        </tr>
+                        {/if}
+                        {#each floorWorkedTodayArray as row}
+                            <tr>
+                                <td>{row.employee.name}</td>
+                                <td>
+                                    <div class="combo-select">
+                                        <button class="left {
+                                            row.workedToday ?
+                                                "selected" : ""
+                                        }" onclick={() => row.workedToday = true}>
+                                            <CheckmarkIcon class="combo-selected-icon"></CheckmarkIcon>
+                                            Yes
+                                        </button>
+                                        <button class="right {
+                                            row.workedToday ?
+                                                "" : "selected"
+                                        }" onclick={() => row.workedToday = false}>
+                                            <CheckmarkIcon class="combo-selected-icon"></CheckmarkIcon>
+                                            No
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                         {/each}
                     </tbody>
                 </table>
             </div>
             <div class="areakitchen">
-                <p>Kitchen Workers</p>
+                <div class="flex" style="justify-content: space-between;">
+                    <p>Kitchen Workers</p>
+                    <p class="fg0">
+                        {kitchenHoursArray.length + kitchenWorkedTodayArray.length} total
+                    </p>
+                </div>
                 <table>
                     <thead>
                         <tr>
@@ -191,33 +247,82 @@ onMount(function () {
                     <tbody>
                         {#each kitchenHoursArray as row}
                             <tr>
-                                <td>{row.name}</td>
-                                <td><input type="text" bind:value={row.hours}></td>
+                                <td>{row.employee.name}</td>
+                                <td><input type="text" bind:value={row.hours}
+                                    style="min-width: 6rem; field-sizing: content;"></td>
+                            </tr>
+                        {/each}
+                        {#if kitchenWorkedTodayArray.length >= 1}
+                        <tr>
+                            <th>Employee</th>
+                            <th>Worked Today?</th>
+                        </tr>
+                        {/if}
+                        {#each kitchenWorkedTodayArray as row}
+                            <tr>
+                                <td>{row.employee.name}</td>
+                                <td>
+                                    <div class="combo-select">
+                                        <button class="left {
+                                            row.workedToday ?
+                                                "selected" : ""
+                                        }" onclick={() => row.workedToday = true}>
+                                            <CheckmarkIcon class="combo-selected-icon"></CheckmarkIcon>
+                                            Yes
+                                        </button>
+                                        <button class="right {
+                                            row.workedToday ?
+                                                "" : "selected"
+                                        }" onclick={() => row.workedToday = false}>
+                                            <CheckmarkIcon class="combo-selected-icon"></CheckmarkIcon>
+                                            No
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                         {/each}
                     </tbody>
                 </table>
             </div>
             <div class="areafood">
-                <p>Ingredient Expenses</p>
-                <table>
+                <div class="flex" style="justify-content: space-between;">
+                    <p>Food Cost</p>
+                    <p class="fg0">
+                        {foodCostsArray.length} {foodCostsArray.length == 1 ?
+                            "item" : "items"}
+                    </p>
+                </div>
+                <div style="border: 0.2rem solid var(--border); border-radius: 0.8rem;">
+                <table style="border: none;">
                     <thead>
                         <tr>
-                            <th>Increases</th>
-                            <th>Decreases</th>
+                            <th>Expenses</th>
+                            <th>Notes</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {#each ingredientExpensesArray as row}
+                        {#each foodCostsArray as row, rowIndex}
                             <tr>
-                                <td><input type="text" bind:value={row.increase}></td>
-                                <td><input type="text" bind:value={row.decrease}></td>
+                                <td style="align-content: start; padding-right: 0px;">
+                                    <div class="flex compact-gap nowrap" style="align-items: center; align-content: center;">
+                                        <span>$</span>
+                                        <input type="text" placeholder="0.00" bind:value={row.cost} style="min-width: 5rem; field-sizing: content;">
+                                    </div>
+                                </td>
+                                <td><textarea style="width: 10rem;" placeholder="Optional note for each expense" bind:value={row.notes}></textarea></td>
+                                    <td style="align-content: start; padding-left: 0px;">
+                                    <button class="icon-only-button" onclick={() => {
+                                        foodCostsArray.splice(rowIndex, 1)
+                                    }}>
+                                        <XMarkIcon></XMarkIcon>
+                                    </button>
+                                </td>
                             </tr>
                         {/each}
                         <tr>
-                            <td>
+                            <td style="padding-right: 0px;">
                                 <button onclick={function () {
-                                    ingredientExpensesArray.push({increase:"",decrease:""})
+                                    foodCostsArray.push({cost:"",notes:""})
                                 }}>
                                     <PlusIcon></PlusIcon>
                                     Add
@@ -226,9 +331,73 @@ onMount(function () {
                         </tr>
                     </tbody>
                 </table>
+                <div style="padding: 0px 0px 1rem 1rem; margin-top: 0.6rem; font-size: 1.1rem;"
+                >Total: ${
+                    foodCostsTotal.toLocaleString('en-US', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2
+                    })
+                }</div>
+                </div>
             </div>
             <div class="areasave">
-                <button>
+                <button onclick={() => {
+                    for (let index = 0; index < floorHoursArray.length; index++) {
+                        if (isNaN(parseFloat(floorHoursArray[index].hours))) {
+                            floorHoursArray[index].hours = 0;
+                        } else {
+                            floorHoursArray[index].hours = parseFloat(
+                                floorHoursArray[index].hours
+                            );
+                        }
+                    }
+                    for (let index = 0; index < kitchenHoursArray.length; index++) {
+                        if (isNaN(parseFloat(kitchenHoursArray[index].hours))) {
+                            kitchenHoursArray[index].hours = 0;
+                        } else {
+                            kitchenHoursArray[index].hours = parseFloat(
+                                kitchenHoursArray[index].hours
+                            );
+                        }
+                    }
+                    const hoursWorked = floorHoursArray.concat(kitchenHoursArray);
+
+                    const workedToday = floorWorkedTodayArray.concat(kitchenWorkedTodayArray);
+                    for (let index = 0; index < foodCostsArray.length; index++) {
+                        foodCostsArray[index].cost = foodCostsArray[index].cost.replaceAll(
+                            ",", "" /* remove commas for thousands before parsing as float */
+                        )
+                        if (isNaN(parseFloat(foodCostsArray[index].cost))) {
+                            foodCostsArray[index].cost = 0;
+                        } else {
+                            foodCostsArray[index].cost = parseFloat(
+                                foodCostsArray[index].cost
+                            );
+                        }
+                    }
+
+                    console.log(JSON.stringify({
+                            date: date.getFullYear() + "-" +
+                                (date.getMonth() + 1).toString().padStart(2, "0") + "-" +
+                                date.getDate().toString().padStart(2, "0"),
+                            hoursWorked: hoursWorked,
+                            workedToday: workedToday,
+                            foodCosts: foodCostsArray
+                    }))
+                    fetch(data.PUBLIC_API_URL + "/days", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + localStorage.getItem("budgeter:auth")
+                        },
+                        body: JSON.stringify({
+                            date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+                            hoursWorked: hoursWorked,
+                            workedToday: workedToday,
+                            foodCosts: foodCostsArray
+                        })
+                    })
+                }}>
                     <CheckmarkIcon></CheckmarkIcon>
                     Save
                 </button>
