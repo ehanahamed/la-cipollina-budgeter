9fab6822c527e7d9db842488fe8b1261ea0737af
Ehan <ehanahamed@ehan.dev>
2025-07-29 14:38:14 -0400

    add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from "$app/state";
+    import { base } from "$app/paths";
+    import BackIcon from "$lib/icons/BackArrow.svelte";
+</script>
+<div class="grid page" style="margin-top: 4rem;">
+    <div class="content">
+        <div class="flex">
+            <a class="button faint" href={base}>
+                <BackIcon></BackIcon>
+                Back
+            </a>
+        </div>
+        <h2>{page.status} {page.error.message}</h2>
+    </div>
+</div>
